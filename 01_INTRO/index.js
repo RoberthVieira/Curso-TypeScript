f54306c99cce499cc5b2d294570b778aaee4d014
Roberth Vieira <135480177+RoberthVieira@users.noreply.github.com>
2025-01-03 20:23:31 -0300

    introdução

---
+++ 01_INTRO/index.js
@@ -0,0 +1,10 @@
+var firstName = 'Roberth';
+var anotherNome = 1;
+var x = true;
+function greeting(name) {
+    console.log('Olá ' + name);
+}
+;
+greeting(firstName);
+//greeting(anotherNome);
+//greeting(x);
