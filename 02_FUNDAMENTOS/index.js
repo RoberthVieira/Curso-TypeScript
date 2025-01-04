722b383654169a27b1b172c1b9815c18994de859
Roberth Vieira <135480177+RoberthVieira@users.noreply.github.com>
2025-01-03 21:30:02 -0300

    Criado a pasta 02Fundamentos

---
+++ 02_FUNDAMENTOS/index.js
@@ -0,0 +1,22 @@
+// 1 - Number
+var n = 10;
+console.log(n);
+n = 16;
+console.log(typeof n);
+var m = 15.458;
+console.log(typeof m);
+console.log(m);
+console.log(m.toPrecision(3));
+// 2 - String
+var text = "Isso é um texto";
+console.log(text.toUpperCase());
+var fullName = "Vieira";
+console.log(typeof fullName);
+text = text + fullName;
+console.log(text);
+// 3 - Boolean
+var a = false;
+console.log(a);
+console.log(typeof a);
+a = true;
+console.log(a);
---
+++ 02_FUNDAMENTOS/index.ts
@@ -0,0 +1,33 @@
+// 1 - Number
+let n: number = 10;
+console.log(n);
+n = 16;
+console.log(typeof n);
+
+const m:number = 15.458;
+console.log(typeof m);
+console.log(m);
+console.log(m.toPrecision(3));
+
+
+
+// 2 - String
+let text: string = "Isso é um texto";
+console.log(text.toUpperCase());
+
+let fullName: string = "Vieira";
+
+console.log(typeof fullName)
+
+text = text +  fullName;
+console.log(text);
+
+
+
+// 3 - Boolean
+let a: boolean = false;
+
+console.log(a);
+console.log(typeof a);
+a = true;
+console.log(a);
