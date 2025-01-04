4cf41ba10cfbdf9cd365c3577f674d5ecc7f149a
Roberth Vieira <135480177+RoberthVieira@users.noreply.github.com>
2025-01-04 17:20:36 -0300

    Fim da seção de fundametos

--- 02_FUNDAMENTOS/index.js
+++ 02_FUNDAMENTOS/index.js
@@ -1,22 +1,29 @@
+"use strict";
 // 1 - Number
-var n = 10;
+let n = 10;
 console.log(n);
 n = 16;
 console.log(typeof n);
-var m = 15.458;
+const m = 15.458;
 console.log(typeof m);
 console.log(m);
 console.log(m.toPrecision(3));
 // 2 - String
-var text = "Isso é um texto";
+let text = "Isso é um texto";
 console.log(text.toUpperCase());
-var fullName = "Vieira";
+let fullName = "Vieira";
 console.log(typeof fullName);
 text = text + fullName;
 console.log(text);
 // 3 - Boolean
-var a = false;
+let a = false;
 console.log(a);
 console.log(typeof a);
 a = true;
 console.log(a);
+// 4 - Annotation e Inference
+const ann = 'test';
+let inf = 'test';
+//inf = 1 vai dar erro por que através do inference o TS ja definiu o tipo de variavel
+// 5 - Compilando automaticamente
+console.log("Compilou automaticamente, pois foi usado comando tsc -w.");
--- 02_FUNDAMENTOS/index.ts
+++ 02_FUNDAMENTOS/index.ts
@@ -31,3 +31,16 @@ console.log(a);
 console.log(typeof a);
 a = true;
 console.log(a);
+
+
+
+// 4 - Annotation e Inference
+const ann: string = 'test';
+
+let inf = 'test';
+//inf = 1 vai dar erro por que através do inference o TS ja definiu o tipo de variavel
+
+
+
+// 5 - Compilando automaticamente
+console.log("Compilou automaticamente, pois foi usado comando tsc -w.")
